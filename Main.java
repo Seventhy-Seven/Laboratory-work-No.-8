f9150257dc0b784819fb83a2ec7c9f013451bee2
Seventhy-Seven <72259094+Seventhy-Seven@users.noreply.github.com>
2020-12-16 00:11:34 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1, null);
+        Node node2 = new Node(2, null);
+        Node node3 = new Node(3, null);
+        Node node4 = new Node(4, null);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Node ref = node1;
+        while (ref != null) {
+            System.out.print(ref.value + " ");
+            ref = ref.next;
+        }
+    }
+}
